245e27f6c749dbdb480c332d537b976301a25ba2
xiaotudui <zhiyaozhou@126.com>
2025-01-19 21:43:22 +0800

    Revert "Update Layout"
    This reverts commit 668148a3c08dc6d3b130bff0db7be8b70f388280.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { PrimeReactProvider } from "primereact/api";
-import "primereact/resources/themes/lara-light-cyan/theme.css";
+
 import './index.css'
 
 import './demos/ipc'
@@ -11,9 +10,7 @@ import './demos/ipc'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <PrimeReactProvider>
-      <App />
-    </PrimeReactProvider>
+    <App />
   </React.StrictMode>,
 )
 
