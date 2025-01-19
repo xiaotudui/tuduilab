7adedf707c5d97d60ef4ad91f6d05ceb4d0c4402
xiaotudui <zhiyaozhou@126.com>
2025-01-19 21:50:09 +0800

    Added Icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import UpdateElectron from '@/components/update'
-import logoVite from './assets/logo-vite.svg'
+import logoVite from './assets/logo2.svg'
 import logoElectron from './assets/logo-electron.svg'
 import './App.css'
 
@@ -9,12 +9,14 @@ function App() {
   return (
     <div className='App'>
       <div className='logo-box'>
-        <a href='www.xiaotudui.com' target='_blank'>
+        <a href='https://www.xiaotudui.com' target='_blank'>
           <img src={logoVite} className='logo vite' alt='Electron + Vite logo' />
-          <img src={logoElectron} className='logo electron' alt='Electron + Vite logo' />
         </a>
       </div>
-      <h1>Electron + Vite + React</h1>
+      <h1>土堆实验室</h1>
+      <p className='read-the-docs'>
+        提供更多有用的工具来简化一些处理。
+      </p>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
