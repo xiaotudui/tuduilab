f7327e076c43773a94f9fddcaad893265f58a49f
xiaotudui <zhiyaozhou@126.com>
2025-01-20 22:33:58 +0800

    Update .debug.script.mjs

--- .vscode/.debug.script.mjs
+++ .vscode/.debug.script.mjs
@@ -2,7 +2,7 @@ import fs from 'node:fs'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { createRequire } from 'node:module'
-import { spawn } from 'node:child_process'
+import { exec } from 'node:child_process'
 
 const pkg = createRequire(import.meta.url)('../package.json')
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
@@ -12,12 +12,19 @@ const envContent = Object.entries(pkg.debug.env).map(([key, val]) => `${key}=${v
 fs.writeFileSync(path.join(__dirname, '.debug.env'), envContent.join('\n'))
 
 // bootstrap
-spawn(
-  // TODO: terminate `npm run dev` when Debug exits.
-  process.platform === 'win32' ? 'npm.cmd' : 'npm',
-  ['run', 'dev'],
-  {
-    stdio: 'inherit',
-    env: Object.assign(process.env, { VSCODE_DEBUG: 'true' }),
-  },
-)
+const command = process.platform === 'win32' ? 'npm.cmd run dev' : 'npm run dev'
+
+exec(command, {
+  stdio: 'inherit',
+  env: Object.assign(process.env, { VSCODE_DEBUG: 'true' }),
+}, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`执行出错: ${error}`)
+    return
+  }
+  if (stderr) {
+    console.error(`stderr: ${stderr}`)
+    return
+  }
+  console.log(`stdout: ${stdout}`)
+})
